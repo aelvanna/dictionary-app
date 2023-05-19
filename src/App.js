c21e9dbdc26f499e60638fbfb75bc8ff21880161
aelvanna <lmarie.fernandez@gmail.com>
2023-05-19 16:11:18 +1000

    updated css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <h1>Dictionary</h1>
         </header>
         <main>
-          <h3>What word do you want to look up?</h3>
+          <h4>What word do you want to look up?</h4>
           <Dictionary />
         </main>
         <footer className="text-center">Coded by Liza Fernandez</footer>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,7 @@
 import React from "react";
+import "./Dictionary.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>

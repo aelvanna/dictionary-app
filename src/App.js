0d2989bdf4bcad07be2dbedbe201ea58189d425f
aelvanna <lmarie.fernandez@gmail.com>
2023-05-19 16:04:11 +1000

    added and adjusted css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header text-center">
+          <h1>Dictionary</h1>
+        </header>
         <main>
+          <h3>What word do you want to look up?</h3>
           <Dictionary />
         </main>
         <footer className="text-center">Coded by Liza Fernandez</footer>

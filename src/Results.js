57025426de24d024b74f07275b5085c6b845cbd2
aelvanna <lmarie.fernandez@gmail.com>
2023-05-22 20:48:42 +1000

    added phonetics

--- src/Results.js
+++ src/Results.js
@@ -3,11 +3,12 @@ import Meaning from "./Meaning";
 import "./Dictionary.css";
 
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <h3>{props.results.phonetic}</h3>
+        <br />
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
